ffd0fc43f5d630a01042b3a4cdb1297a1b9e1f30
tatjam <tatjam02@gmail.com>
2023-12-09 15:38:55 +0100

    Renderer structure changes

--- res/demo/entrypoint.lua
+++ res/demo/entrypoint.lua
@@ -1,29 +1,51 @@
+-- Includes
 local logger = require("logger")
 local assets = require("assets")
 local renderer = require("renderer")
 local glm = require("glm")
 local test_plugin = require("test_plugin")
+
 require("universe")
 
+local cameras = require("core:camera_util.lua")
+
+-- Create some assets
 local skybox = renderer.skybox.new(assets.get_cubemap("core:notex.png"):move())
 local sunlight = renderer.sun_light.new(hgr.renderer.quality.sun_terrain_shadow_size, hgr.renderer.quality.sun_shadow_size)
 
-local cameras = require("core:camera_util.lua")
+local obj
+
+-- Initialization
+
+-- This function is only called in the entrypoint, and is used for setting up
+-- the game state the first time it launches. Afterwards, a save will be loaded
+-- and only load will be called
+function first_load()
+    -- Setup the planetary system
+    logger.info("Setting up system")
+
+end
 
 function load()
     logger.info("Hello from entrypoint")
 
-    local ent = hgr.universe:create_object("demo:test_object.lua")
-    hgr.renderer:add_drawable(skybox)
-    hgr.renderer:add_table_as_drawable(ent.lua)
-    hgr.renderer:add_light(sunlight)
+    -- Create an object
+    obj = hgr.universe:create_object("demo:test_object.lua")
 
+    -- Add drawables and lights
+
+    -- Call a C++ plugin
     test_plugin.hello_plugin()
-
-    -- Also add the skybox
 end
 
+-- Updating
+
+-- Rendering
+
 function render()
+    hgr.renderer:add_drawable(skybox)
+    hgr.renderer:add_drawable(obj.lua)
+    hgr.renderer:add_light(sunlight)
     hgr.renderer:render()
 end
 
